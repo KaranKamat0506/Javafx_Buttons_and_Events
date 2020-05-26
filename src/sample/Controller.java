f269be978a1a068922940e44df3eb4650f397696
KaranKamat0506 <karankamat2000@gmail.com>
2020-05-26 20:48:43 +0530

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,24 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @FXML
+    private Label greetingLabel;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+    public void showGreeting(ActionEvent event){
+//        System.out.println("Hello World");
+        greetingLabel.setText("Hello World");
+    }
+}
