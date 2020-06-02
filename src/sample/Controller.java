41b572fd17a3c70eb163a3abce427c484a34dd2a
KaranKamat0506 <karankamat2000@gmail.com>
2020-06-02 19:00:46 +0530

    Reading from one text field and writing it to another text field.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -13,12 +14,20 @@ public class Controller implements Initializable {
     @FXML
     private Label greetingLabel;
 
+    @FXML
+    private TextField t1,t2;
+
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
     }
     public void showGreeting(ActionEvent event){
         System.out.println("Hello World");
-        greetingLabel.setText("Hello World");
+        //greetingLabel.setText("Hello World");
+        String input=t1.getText();
+        t2.setText(input);
+
+
     }
 }
